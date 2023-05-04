173b633a7c86a098afbd12f17dcca3b1bdc62835
ayoub <ayoubbenllouti@gmail.com>
2023-05-04 13:35:22 -0700

    task number six 0x14

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+**(ALX PROGRAME 0x14):
+**flip_bits - counts the number of bits to chan
+**/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
+	}
+
+	return (count);
+}
+
